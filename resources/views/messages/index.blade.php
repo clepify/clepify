49b1178a33ffddabe58bed96ef296b575a403f7e
caradhiANA <aleganaufal@gmail.com>
2024-04-25 09:26:31 +0700

    feat: add messages menu view and configure notification ui

---
+++ resources/views/messages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title', 'Messages')
+
+@section('breadcrumb')
+  <ol class="breadcrumb my-0 ms-2">
+    <li class="breadcrumb-item"><a href="{{ route('dashboard') }}"><span>Dashboard</span></a></li>
+    <li class="breadcrumb-item active"><span>Messages</span></li>
+  </ol>
+@endsection
+
+@section('content')
+Ini Message
+@endsection
